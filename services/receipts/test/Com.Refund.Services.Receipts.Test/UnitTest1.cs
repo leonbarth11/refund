5666d8105b188add0c90697f115e0af42a6941e6
Leon Barth <leonbarth1993@googlemail.com>
2022-12-26 00:32:35 +0100

    Create project skeletons for receipts service

---
+++ services/receipts/test/Com.Refund.Services.Receipts.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Com.Refund.Services.Receipts.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
